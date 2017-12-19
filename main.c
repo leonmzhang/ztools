bfbfb28141a1ebabbc46355a6087c289dbfd56b2
mingzhang2 <mingzhang2@iflytek.com>
2017-12-19 11:52:08 +0800

    init project

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+void read_sector(const char *path)
+{
+    
+
+    return;
+}
+
+int main()
+{
+    const char *disk = "/dev/sdc";
+
+    read_sector(disk);
+
+    return 0;
+}
